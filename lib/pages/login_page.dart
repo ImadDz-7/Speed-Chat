4db95d892fbb300d63bbc1faa0e2bcb89512ea61
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-07-21 22:09:34 +0000

    Add Image & title and Text field

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -9,7 +9,40 @@ class LoginPage extends StatelessWidget {
       backgroundColor: const Color(0xFF2B475E),
       body: Column(
         children: [
-          
+          Image.asset(
+            'assets/images/fastchat.png',
+            width: 220,
+          ),
+          const Text(
+            'Speed Chat',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+              fontFamily: 'Pacifico',
+            ),
+          ),
+          const Text(
+            'LOGIN',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
+           TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.white,
+                ),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          // Text('Speed Chat'),
         ],
       ),
     );
