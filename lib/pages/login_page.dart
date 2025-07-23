582dff6e6e06106a9f81e88aeb2ed4d7be7f937d
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-07-23 20:46:02 +0000

    Login page UI done

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -9,37 +9,57 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF2B475E),
-      body: Column(
-        children: [
-          Image.asset(
-            'assets/images/fastchat.png',
-            width: 220,
-          ),
-          const Text(
-            'Speed Chat',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 30,
-              fontFamily: 'Pacifico',
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8),
+        child: Column(
+          children: [
+            const Spacer(flex: 1),
+            Image.asset(
+              'assets/images/fastchat.png',
+              width: 220,
             ),
-          ),
-          const Text(
-            'LOGIN',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 20,
+            const Text(
+              'Speed Chat',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+                fontFamily: 'Pacifico',
+              ),
             ),
-          ),
-          const CustomTextField(hintText: 'Email'),
-          const CustomTextField(hintText: 'Password'),
-          const CustomButton(text: 'Login'),
-          Row(
-            children: [
-              Text('don\'t have an account ', style: TextStyle(color: Colors.white),),
-              Text('Register', style: TextStyle(color: Colors.white),),
-            ],
-          ),
-        ],
+            Row(
+              children: const [
+                Text(
+                  'LOGIN',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            const CustomTextField(hintText: 'Email'),
+            const SizedBox(height: 10),
+            const CustomTextField(hintText: 'Password'),
+            const SizedBox(height: 30),
+            const CustomButton(text: 'Login'),
+            const SizedBox(height: 15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'don\'t have an account? ',
+                  style: TextStyle(color: Colors.white),
+                ),
+                Text(
+                  'Register',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+            const Spacer(flex: 2),
+          ],
+        ),
       ),
     );
   }
