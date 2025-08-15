be176a9856b984965e865c9a9a21febf276c91f4
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-08-15 18:23:34 +0000

    Refactor custom text field and using TextFormField instead of TextField

--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -7,7 +7,12 @@ class CustomTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      validator: (data){
+        if(data!.isEmpty){
+          return 'field is required';
+        }
+      },
       onChanged: onChanged,
       style: const TextStyle(color: Colors.white),
       decoration: InputDecoration(
