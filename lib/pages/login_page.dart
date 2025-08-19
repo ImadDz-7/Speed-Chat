11f2c2846ce76bb7852b6b6b7cc01ff1ff0efcee
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-08-19 11:52:58 +0000

    Add ModalProgressHud

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:speed_chat/constants.dart';
 import 'package:speed_chat/pages/register_page.dart';
 import 'package:speed_chat/widgets/custom_button.dart';
@@ -14,76 +15,79 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   bool isLoading = false;
-  
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: kPrimaryColor,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8),
-        child: ListView(
-          children: [
-            const SizedBox(height: 50),
-            Image.asset(
-              'assets/images/fastchat.png',
-              width: 220,
-              height: 130,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                 Text(
-                  'Speed Chat',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                    fontFamily: 'Pacifico',
+    return ModalProgressHUD(
+      inAsyncCall: isLoading,
+      child: Scaffold(
+        backgroundColor: kPrimaryColor,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: ListView(
+            children: [
+              const SizedBox(height: 50),
+              Image.asset(
+                'assets/images/fastchat.png',
+                width: 220,
+                height: 130,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                   Text(
+                    'Speed Chat',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontFamily: 'Pacifico',
+                    ),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              children: const [
-                Text(
-                  'LOGIN',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
+                ],
+              ),
+              Row(
+                children: const [
+                  Text(
+                    'LOGIN',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
-             CustomTextField(hintText: 'Email'),
-            const SizedBox(height: 10),
-             CustomTextField(hintText: 'Password'),
-            const SizedBox(height: 30),
-            CustomButton(
-              onTap:(){
-
-              } 
-              ,
-              text: 'Login'),
-            const SizedBox(height: 15),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'Don\'t have an account? ',
-                  style: TextStyle(color: Colors.white),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.pushNamed(context, RegisterPage.id);
-                  },
-                  child: const Text(
-                    'Register',
+                ],
+              ),
+              const SizedBox(height: 20),
+               CustomTextField(hintText: 'Email'),
+              const SizedBox(height: 10),
+               CustomTextField(hintText: 'Password'),
+              const SizedBox(height: 30),
+              CustomButton(
+                onTap:(){
+    
+                } 
+                ,
+                text: 'Login'),
+              const SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Don\'t have an account? ',
                     style: TextStyle(color: Colors.white),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(context, RegisterPage.id);
+                    },
+                    child: const Text(
+                      'Register',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
