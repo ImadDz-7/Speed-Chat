2b7cc47e8e88fbbbf1b6798f61f70b4e75747b74
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-08-19 12:41:18 +0000

    Add Form & handling exceptions

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -19,6 +19,7 @@ class _LoginPageState extends State<LoginPage> {
   bool isLoading = false;
   String? email;
   String? password;
+  GlobalKey<FormState> formKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
@@ -28,95 +29,111 @@ class _LoginPageState extends State<LoginPage> {
         backgroundColor: kPrimaryColor,
         body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8),
-          child: ListView(
-            children: [
-              const SizedBox(height: 50),
-              Image.asset(
-                'assets/images/fastchat.png',
-                width: 220,
-                height: 130,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Speed Chat',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 30,
-                      fontFamily: 'Pacifico',
+          child: Form(
+            key: formKey,
+            child: ListView(
+              children: [
+                const SizedBox(height: 50),
+                Image.asset(
+                  'assets/images/fastchat.png',
+                  width: 220,
+                  height: 130,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Speed Chat',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontFamily: 'Pacifico',
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                children: const [
-                  Text(
-                    'LOGIN',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
+                  ],
+                ),
+                Row(
+                  children: const [
+                    Text(
+                      'LOGIN',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              CustomTextField(
-                onChanged: (data) {
-                  email = data;
-                },
-                hintText: 'Email',
-              ),
-              const SizedBox(height: 10),
-              CustomTextField(
-                onChanged: (data) {
-                  password = data;
-                },
-                hintText: 'Password',
-              ),
-              const SizedBox(height: 30),
-              CustomButton(
-                onTap: () async {
-                  try {
-                    var auth = FirebaseAuth.instance;
-                    UserCredential user = await auth.signInWithEmailAndPassword(
-                      email: email!,
-                      password: password!,
-                    );
-                    showSnackBar(
-                      context: context,
-                      message: 'Login successfully',
-                    );
-                  } on FirebaseAuthException catch (e) {
-                    // if (e.code == 'user-not-found') {
-                    //   print('No user found for that email.');
-                    // } else if (e.code == 'wrong-password') {
-                    //   print('Wrong password provided for that user.');
-                    // }
-                  }
-                },
-                text: 'Login',
-              ),
-              const SizedBox(height: 15),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Don\'t have an account? ',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushNamed(context, RegisterPage.id);
-                    },
-                    child: const Text(
-                      'Register',
+                  ],
+                ),
+                const SizedBox(height: 20),
+                CustomTextField(
+                  onChanged: (data) {
+                    email = data;
+                  },
+                  hintText: 'Email',
+                ),
+                const SizedBox(height: 10),
+                CustomTextField(
+                  onChanged: (data) {
+                    password = data;
+                  },
+                  hintText: 'Password',
+                ),
+                const SizedBox(height: 30),
+                CustomButton(
+                  onTap: () async {
+                    try {
+                      var auth = FirebaseAuth.instance;
+                      UserCredential user = await auth.signInWithEmailAndPassword(
+                        email: email!,
+                        password: password!,
+                      );
+                      showSnackBar(
+                        context: context,
+                        message: 'Login successfully',
+                      );
+                    } on FirebaseAuthException catch (e) {
+                      if (e.code == 'user-not-found') {
+                        showSnackBar(
+                          context: context,
+                          message: 'No user found for that email.',
+                        );
+                      } else if (e.code == 'wrong-password') {
+                        showSnackBar(
+                          context: context,
+                          message: 'Wrong password provided for that user.',
+                        );
+                      }
+                    } catch (e) {
+                      showSnackBar(
+                        context: context,
+                        message: 'Oops there was an error, please try again!',
+                      );
+                    }
+                    isLoading = false;
+                    setState(() {});
+                  },
+                  text: 'Login',
+                ),
+                const SizedBox(height: 15),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Don\'t have an account? ',
                       style: TextStyle(color: Colors.white),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, RegisterPage.id);
+                      },
+                      child: const Text(
+                        'Register',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
