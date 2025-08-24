c3b996e0faf30ea9788264ee621a0fe321caf6b3
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-08-24 22:46:10 +0000

    Read Data from firebase using FutureBuilder

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -13,62 +13,67 @@ class ChatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: kPrimaryColor,
-        centerTitle: true,
-        automaticallyImplyLeading: false,
-        title: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              kLogo,
-              height: 50,
-            ),
-            const Text('Chat'),
-          ],
-        ),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemBuilder: ((context, index) {
-                return ChatBubble();
-              }),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: TextField(
-              controller: controller,
-              onSubmitted: (data) {
-                messages.add(
-                  {
-                    'message': data,
-                  },
-                );
-                controller.clear();
-              },
-              decoration: InputDecoration(
-                  hintText: 'Send Message',
-                  suffixIcon: Icon(
-                    Icons.send,
-                    color: kPrimaryColor,
+    return FutureBuilder<DocumentSnapshot>(
+        future: messages.doc('QayZD6jJJzl66mzwWpCy').get(),
+        builder: (context, snapshot) {
+          print(snapshot.data!['message']);
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: kPrimaryColor,
+              centerTitle: true,
+              automaticallyImplyLeading: false,
+              title: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.asset(
+                    kLogo,
+                    height: 50,
                   ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide: BorderSide(
-                      color: kPrimaryColor,
-                    ),
-                  )),
+                  const Text('Chat'),
+                ],
+              ),
             ),
-          ),
-        ],
-      ),
-    );
+            body: Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemBuilder: ((context, index) {
+                      return ChatBubble();
+                    }),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: TextField(
+                    controller: controller,
+                    onSubmitted: (data) {
+                      messages.add(
+                        {
+                          'message': data,
+                        },
+                      );
+                      controller.clear();
+                    },
+                    decoration: InputDecoration(
+                        hintText: 'Send Message',
+                        suffixIcon: Icon(
+                          Icons.send,
+                          color: kPrimaryColor,
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16),
+                          borderSide: BorderSide(
+                            color: kPrimaryColor,
+                          ),
+                        )),
+                  ),
+                ),
+              ],
+            ),
+          );
+        });
   }
 }
